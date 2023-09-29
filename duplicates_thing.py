516178a0a636376f1f73522c1268da88fcdbd3ec
storm <stormurbiz@gmail.com>
2023-09-29 15:10:55 +0700

    fix3

--- duplicates_thing.py
+++ duplicates_thing.py
@@ -8,14 +8,13 @@ def duplicateChars(word):
         else:
             dictRepeats[el] = 1
     
-    new_dict = {}
+
     elements_repeated = 0
 
     for el in dictRepeats:
         if dictRepeats[el] == 1:
             pass
         else:
-            new_dict[el] = dictRepeats[el]
             elements_repeated += 1
     
     return elements_repeated
